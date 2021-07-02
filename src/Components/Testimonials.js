0fb6097e081e5bbdf2c0d2d9fb66730192a850fd
codemaker6 <amang5226@gmail.com>
2021-07-02 20:29:16 +0530

    added userdata components

---
+++ src/Components/Testimonials.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Data from "./Data";
+
+const Testimonials=()=>{
+    return(
+        <>
+        <section className="section">
+            <div className="section-block">
+                <div className="data-block">
+                   <Data></Data>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
+export default Testimonials;
---
+++ src/Components/Userdata.js
@@ -0,0 +1,21 @@
+import React from "react";
+const Userdata = () => {
+  return (
+    <>
+      <div>
+        <h1 className="heading">84 % reduction in image bandwidth</h1>
+        <p className="paragraph-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non
+          fringilla leo. Phasellus dui erat, ultrices sollicitudin ante <br />{" "}
+          quis, convallis varius odio. Es hat die Art und Weise, wie ich die
+          Website benutzt habe, verändert. Mit dem Produkt
+          <br /> können Sie alles erstellen, was Sie sich vorstellen.
+        </p>
+        <p className="author-name">
+          Heisenberg,<span className="designation">CTO,Website.com</span>
+        </p>
+      </div>
+    </>
+  );
+};
+export default Userdata;
